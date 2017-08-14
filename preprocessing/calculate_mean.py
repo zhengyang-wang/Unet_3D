correct the implementation of data normalization

preprocessing/calculate_mean.py
@@ -6,10 +6,6 @@ import nibabel as nib
 import numpy as np
 import os
 
-# arguments
-margin = 16 # training_patch_size / 2
-raw_data_dir = '../data'
-
 def cut_edge(data, keep_margin):
     '''
     function that cuts zero edge
@@ -62,7 +58,7 @@ def get_cut_size(data_path):
         f = os.path.join(data_path, subject_name+'T1.hdr')
         img = nib.load(f)
         inputs_tmp = img.get_data()
-        D_s, D_e, H_s, H_e, W_s, W_e = cut_edge(inputs_tmp, margin)
+        D_s, D_e, H_s, H_e, W_s, W_e = cut_edge(inputs_tmp, 0) # set how many zero margins to keep
         list_D_s.append(D_s)
         list_D_e.append(D_e)
         list_H_s.append(H_s)
@@ -117,5 +113,44 @@ def get_mean(data_path):
     print("T1 cut mean: ", np.mean(cut_mean1_per_sub))
     print("T2 cut mean: ", np.mean(cut_mean2_per_sub))
 
+def get_mean_and_std(data_path): # add std calculation. Slow!
+    data1 = []
+    data2 = []
+    cut_data1 = []
+    cut_data2 = []
+
+    D_s, D_e, H_s, H_e, W_s, W_e = get_cut_size(data_path)
+    cut_size = (D_e-D_s+1) * (H_e-H_s+1) * (W_e-W_s+1)
+    # print(cut_size)
+
+    for i in range(1, 10+1):
+        subject_name = 'subject-%d-' % i
+        f_T1 = os.path.join(data_path, subject_name+'T1.hdr')
+        img_T1 = nib.load(f_T1)
+        f_T2 = os.path.join(data_path, subject_name+'T2.hdr')
+        img_T2 = nib.load(f_T2)
+
+        inputs_tmp_T1 = img_T1.get_data()
+        inputs_tmp_T2 = img_T2.get_data()
+
+        D, H, W, C = inputs_tmp_T1.shape
+        original_size = D * H * W
+
+        data1.append(inputs_tmp_T1)
+        data2.append(inputs_tmp_T2)
+
+        cut_data1.append(inputs_tmp_T1[D_s : D_e+1, H_s : H_e+1, W_s : W_e+1])
+        cut_data2.append(inputs_tmp_T2[D_s : D_e+1, H_s : H_e+1, W_s : W_e+1])
+
+    print("T1 mean: ", np.mean(data1))
+    print("T2 mean: ", np.mean(data2))
+    print("T1 std: ", np.std(data1))
+    print("T2 std: ", np.std(data2))
+    print("T1 cut mean: ", np.mean(cut_data1))
+    print("T2 cut mean: ", np.mean(cut_data2))
+    print("T1 cut std: ", np.std(cut_data1))
+    print("T2 cut std: ", np.std(cut_data2))
+
 if __name__ == '__main__':
-    get_mean(raw_data_dir)
+    # get_mean('../data')
+    get_mean_and_std('../data')

preprocessing/generate_h5.py
@@ -4,17 +4,16 @@ import os
 # import matplotlib.pyplot as plt
 import h5py
 
-# copy from results of calculate_mean.py
-MEAN = np.array((57.9034515187, 69.5360292781), dtype=np.float32) # 31.6906890728, 38.0572250084
-SIZE = (0, 143, 9, 191, 69, 215) # (0, 143, 0, 195, 0, 255)
+MEAN = np.array((100.913811861, 121.187003401), dtype=np.float32) # 31.6906890728, 38.0572250084
+STD = np.array((150.078421236, 173.828472883), dtype=np.float32) # 96.2650255072, 112.484409158
+SIZE = (9, 136, 25, 175, 85, 199) # (0, 143, 0, 195, 0, 255)
 
-# arguments
-SUBSTRACT_MEAN = False
-AUGMENT = True
+SUBSTRACT_MEAN = True
+AUGMENT = False
 NORMALIZE = True
 
 data_path = '../data'
-target_path = '../h5_data_AMN'
+target_path = '../h5_data_SN'
 '''    
 h5_data_[S][A][M][N]
     S: mean substraction
@@ -164,7 +163,7 @@ def build_h5_dataset(data_path, target_path):
             inputs -= MEAN
 
         if NORMALIZE:
-            inputs /= 1000.
+            inputs /= STD
 
         dataset['X'][i] = inputs
         dataset['Y'][i] = labels